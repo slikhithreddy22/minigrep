864b6df4a59521c99b5e3a5afa3d64c44dfd353f
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-01-22 13:49:07 +0530

    read args and the file contents mentioned in args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("searching for query {}", query);
+    println!("in file_path, {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("should have able to read");
+    println!("with context: \n {contents}");
 }
