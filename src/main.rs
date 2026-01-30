2b0f4cbef80f13bcd249f3d525db27626bcd4007
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-01-31 00:10:04 +0530

    refractor : improve argument parsing and search logic

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didnt get a quey string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didnt got a file_path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-
         Ok(Config {
             query,
             file_path,
@@ -43,9 +45,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
